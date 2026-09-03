10631a0e6cd29268e917ee8b4cdc120cd09565b7
theretrogeekyt-dev <joshuamichael8841@gmail.com>
2026-09-03 08:41:34 -0500

    chore: bump version to 10033.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.tileforge.xqpkm"
     minSdk = 24
     targetSdk = 36
-    versionCode = 10032
-    versionName = "10032.0"
+    versionCode = 10033
+    versionName = "10033.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
